f7729fdb5a6d42d50175c186b02037d15de3b332
291685399 <291685399@qq.com>
2019-06-23 21:05:26 +0800

    添加@EnableDiscoveryClient注解

--- springcloud-eureka03/eureka-consumer03/src/main/java/com/wyj/EurekaConsumer03Application.java
+++ springcloud-eureka03/eureka-consumer03/src/main/java/com/wyj/EurekaConsumer03Application.java
@@ -2,8 +2,10 @@ package com.wyj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EurekaConsumer03Application {
 
     public static void main(String[] args) {
