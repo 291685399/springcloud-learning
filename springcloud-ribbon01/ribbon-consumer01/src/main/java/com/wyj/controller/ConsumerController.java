621308c4827f2d3979df59a1f902f2500419bd66
291685399 <291685399@qq.com>
2019-07-02 19:53:59 +0800

    修改类名称

--- springcloud-ribbon01/ribbon-consumer01/src/main/java/com/wyj/controller/ConsumerController.java
+++ springcloud-ribbon01/ribbon-consumer01/src/main/java/com/wyj/controller/ConsumerController.java
@@ -2,7 +2,6 @@ package com.wyj.controller;
 
 import com.wyj.entity.po.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,10 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-
 @Controller
-public class CustomerController {
+public class ConsumerController {
 
     @Autowired
     private RestTemplate restTemplate;
