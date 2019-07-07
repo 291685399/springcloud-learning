7e556bd725986748a58350bd20020992894a6d0c
291685399 <291685399@qq.com>
2019-07-07 21:18:17 +0800

    添加feign远程调用

---
+++ springcloud-feign01/feign-provider01/src/main/java/com/wyj/controller/ProviderController.java
@@ -0,0 +1,24 @@
+package com.wyj.controller;
+
+import com.wyj.entity.po.User;
+import com.wyj.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ProviderController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/findById/{id}")
+    @ResponseBody
+    public User findById(@PathVariable int id) throws InterruptedException {
+        User user = userService.findById(id);
+        return user;
+    }
+
+}
